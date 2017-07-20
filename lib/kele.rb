157704cda2468cbd86154d5a77414ed1176c030d
Muhammad <daudjaved1@gmail.com>
2017-07-20 20:30:12 +0000

    first fix

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,9 @@
-Class Kele
-  include HTTParty
+require 'httparty'
+
+ class Kele
+   include HTTParty
   
-  def initialize (email , password)
-      self.class.post("https://www.bloc.io/api/v1/session", body: { "email": email, "password": password })
-  end  
+   def initialize(email , password)
+       self.class.post("https://www.bloc.io/api/v1/session", body: { "email": email, "password": password })
+   end  
+ end
