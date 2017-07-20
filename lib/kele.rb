69025e5ed71cb531e88c68b94de1c462e84e1dfb
Muhammad <daudjaved1@gmail.com>
2017-07-20 17:37:11 +0000

    revision

---
+++ lib/kele.rb
@@ -0,0 +1,6 @@
+Class Kele
+  include HTTParty
+  
+  def initialize (email , password)
+      self.class.post("https://www.bloc.io/api/v1/session", body: { "email": email, "password": password })
+  end  
