135d7777b68ff1b87d5262ff97890ea73808c652
Muhammad <daudjaved1@gmail.com>
2017-07-20 23:24:04 +0000

    retrieve

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,4 +6,8 @@ require 'httparty'
    def initialize(email , password)
        self.class.post("https://www.bloc.io/api/v1/session", body: { "email": email, "password": password })
    end  
+   def get_me
+       response = self.class.get(url, headers: { "authorization" => @auth_token })
+       @user_data = JSON.parse(response.body)
+   end
  end
