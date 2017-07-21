61ac2066f3cffded48776721fbe6b69bfec357b8
Muhammad <daudjaved1@gmail.com>
2017-07-21 00:16:27 +0000

    auth token fix

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,7 +4,10 @@ require 'httparty'
    include HTTParty
   
    def initialize(email , password)
-       self.class.post("https://www.bloc.io/api/v1/session", body: { "email": email, "password": password })
+       response = self.class.post("https://www.bloc.io/api/v1/session", body: { "email": email, "password": password })
+       p response
+       raise "Invalid email or password" if response.code == 404
+       @auth_token = response["auth_token"]
    end  
    def get_me
        response = self.class.get(url, headers: { "authorization" => @auth_token })
